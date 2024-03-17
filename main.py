503b3e09d939d0f813a8b9243f585062858fa92e
barndles <barndles@gmail.com>
2024-03-17 18:29:29 +0000

    mediafool refactor complete!!! yay!!

--- main.py
+++ main.py
@@ -7,18 +7,18 @@ import numpy as np
 from openpyxl import load_workbook
 import requests as rs
 
-xlsx_url="https://docs.google.com/spreadsheets/d/1WCC6GrLwaDmcQtINrCU2SEmjKpBJcWqX5AWDenl4hvY/export?format=xlsx&id=1WCC6GrLwaDmcQtINrCU2SEmjKpBJcWqX5AWDenl4hvY"
+def load_sheet():
+    xlsx_url="https://docs.google.com/spreadsheets/d/1WCC6GrLwaDmcQtINrCU2SEmjKpBJcWqX5AWDenl4hvY/export?format=xlsx&id=1WCC6GrLwaDmcQtINrCU2SEmjKpBJcWqX5AWDenl4hvY"
 
-res = rs.get(url=xlsx_url)
-open('maps_and_descriptors.xlsx', 'wb+').write(res.content)
+    res = rs.get(url=xlsx_url)
+    open('maps_and_descriptors.xlsx', 'wb+').write(res.content)
 
-wb = load_workbook(filename="maps_and_descriptors.xlsx")
-sheet = wb.active
-
-maps = sheet['A']
-descriptors = sheet['B']
-
+    wb = load_workbook(filename="maps_and_descriptors.xlsx")
+    sheet = wb.active
+    return sheet
 def random_map():
+    sheet = load_sheet()
+    maps = sheet['A']
     map = None # Pick a random map, if it's "None", Try again
     while map == None:
         map = maps[np.random.randint(0,len(maps))].value
@@ -26,12 +26,17 @@ def random_map():
             break
     return map
 def random_descriptor():
+    sheet = load_sheet()
+    descriptors = sheet['B']
     descriptor = None # Pick a random map, if it's "None", Try again
     while descriptor == None:
-        descriptor = descriptors[np.random.randint(0,len(maps))].value
+        descriptor = descriptors[np.random.randint(0,len(descriptors))].value
         if descriptor != None:
             break
     return descriptor
+
+
+load_sheet()
 
 bot = commands.Bot()
 intents = discord.Intents.default()
@@ -57,8 +62,7 @@ async def mapidea(ctx):
     else:
         await ctx.respond("map idea: " + random_map() + " but " + random_descriptor())
     
-    res = rs.get(url=xlsx_url)
-    open('maps_and_descriptors.xlsx', 'wb+').write(res.content)
+    load_sheet()
 
 
 @bot.slash_command(name="glueidea", description = "generate a fun new glue idea!!!!!!!!")
